6276088f14f906cd56eaedf55b4a5b6a682a01d0
Ishika Ranjan <ishikaranjan8@gmail.com>
2023-03-19 21:08:59 +0530

    database

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require("express");
 const bodyParser= require("body-parser");
 const mongoose = require("mongoose");
@@ -14,7 +15,7 @@ main().catch(err => console.log(err));
 
 async function main() {
     mongoose.set("strictQuery", false);
-    await mongoose.connect('mongodb://127.0.0.1:27017/todoListDB');
+    await mongoose.connect(process.env.MONGOOSE);
 }
 
 const itemsSchema = new mongoose.Schema({
